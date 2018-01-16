bc180893f9a7ef80163ee6562c5455425add000b
Emin Arazli <emin.arazli@gmail.com>
2018-01-16 13:39:46 +0100

    DLN-219 #time 1h 30m #comment Implement all post requests generically

--- src/beehive_database/core.clj
+++ src/beehive_database/core.clj
@@ -14,6 +14,32 @@
     (slurp (get-in ctx [:request :body]))
     :key-fn keyword))
 
+(defn post-default [post-fn]
+  {:allowed-methods       [:post]
+   :available-media-types ["application/json"]
+   :processable?          (fn [ctx]
+                            (let [data (extract-json ctx)]
+                              {::data data}))
+   :post!                 (fn [ctx]
+                            (let [data (::data ctx)]
+                              (post-fn data)))})
+
+(l/defresource hives []
+               (post-default
+                 #(t/add-hive
+                    (:address %)
+                    (:xcoord %)
+                    (:ycoord %)
+                    (:name %))))
+
+(l/defresource hives []
+               (post-default
+                 #(t/add-hive
+                    (:address %)
+                    (:xcoord %)
+                    (:ycoord %)
+                    (:name %))))
+
 (c/defroutes rest-routes
              (c/GET "/hives" [& ids]
                (l/resource
@@ -28,7 +54,11 @@
              (c/GET "/hives/reachable/:id1/:id2" [id1 id2]
                (l/resource
                  :available-media-types ["application/json"]
-                 :handle-ok (str (q/is-reachable (read-string id1) (read-string id2) (d/db)))))
+                 :handle-ok (str
+                              (q/is-reachable
+                                (read-string id1)
+                                (read-string id2)
+                                (d/db)))))
 
              (c/GET "/hops" [& ids]
                (l/resource
@@ -65,50 +95,87 @@
                  :available-media-types ["application/json"]
                  :handle-ok (q/get-all :customer ids (d/db))))
 
+             (c/GET "/types" [& ids]
+               (l/resource
+                 :available-media-types ["application/json"]
+                 :handle-ok (q/get-all :dronetype ids (d/db))))
+
              (c/POST "/hives" []
                (l/resource
-                 :allowed-methods [:post]
-                 :available-media-types ["application/json"]
-                 :processable? (fn [ctx]
-                                 (let [data (extract-json ctx)]
-                                   {::data data}))
-                 :post! (fn [ctx]
-                          (let [data (::data ctx)]
-                            (t/add-hive
-                              (data :address)
-                              (data :xcoord)
-                              (data :ycoord)
-                              (data :name))))))
+                 (post-default
+                   #(t/add-hive
+                      (:address %)
+                      (:xcoord %)
+                      (:ycoord %)
+                      (:name %)))))
 
              (c/POST "/drones" []
                (l/resource
-                 :allowed-methods [:post]
-                 :available-media-types ["application/json"]
-                 :processable? (fn [ctx]
-                                 (let [data (extract-json ctx)]
-                                   (println data)
-                                   {::data data}))
-                 :post! (fn [ctx]
-                          (let [data (::data ctx)]
-                            (t/add-drone
-                              (data :hiveid)
-                              (data :name)
-                              (data :range)
-                              (data :status))))))
+                 (post-default
+                   #(t/add-drone
+                      (:hiveid %)
+                      (:name %)
+                      (:range %)
+                      (:status %)))))
 
              (c/POST "/routes" []
                (l/resource
-                 :allowed-methods [:post]
-                 :available-media-types ["application/json"]
-                 :post! (fn [ctx]
-                          (let [body (slurp (get-in ctx [:request :body]))]
-                            {::ctx body}))))
+                 (post-default
+                   #(t/add-route
+                      (:hops %)
+                      (:origin %)))))
 
              (c/POST "/orders" []
                (l/resource
-                 :allowed-methods [:post]
-                 :available-media-types ["application/json"]
-                 :post! (fn [ctx])))
+                 (post-default
+                   #(t/add-order
+                      (:shop %)
+                      (:customer %)
+                      (:route %)))))
+
+             (c/POST "/building" []
+               (l/resource
+                 (post-default
+                   #(t/add-building
+                      (:address %)
+                      (:xcoord %)
+                      (:ycoord %)))))
+
+             (c/POST "/shop" []
+               (l/resource
+                 (post-default
+                   #(t/add-shop
+                      (:address %)
+                      (:xcoord %)
+                      (:ycoord %)
+                      (:name %)))))
+
+             (c/POST "/customer" []
+               (l/resource
+                 (post-default
+                   #(t/add-customer
+                      (:address %)
+                      (:xcoord %)
+                      (:ycoord %)
+                      (:name %)))))
+
+             (c/POST "/hop" []
+               (l/resource
+                 (post-default
+                   #(t/add-hop
+                      (:drone %)
+                      (:start %)
+                      (:end %)))))
+
+             (c/POST "/dronetype" []
+               (l/resource
+                 (post-default
+                   #(t/add-drone-type
+                      (:name %)
+                      (:range %)
+                      (:speed %)
+                      (:chargetime %)
+                      (:default %)))))
 
              (c/PUT "/routes" []
                (l/resource
