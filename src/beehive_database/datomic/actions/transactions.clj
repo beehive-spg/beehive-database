1aff052db563eeac6aa279c53068f81f371267e2
Emin Arazli <emin.arazli@gmail.com>
2018-01-29 11:57:40 +0100

    DLN-219 #time 15m #comment Insert default hives via add-hive to preserve connectiions, add reachable route

--- src/beehive_database/datomic/actions/transactions.clj
+++ src/beehive_database/datomic/actions/transactions.clj
@@ -109,5 +109,5 @@
 
 (add-drone-type "large" 5000 15 1800 true)
 
-@(d/transact conn
-             (read-string (slurp "data.edn")))
+(doseq [hive (clojure.edn/read-string (slurp "data.edn"))]
+   (add-hive (:building/address hive) (:building/xcoord hive) (:building/ycoord hive) (:hive/name (:building/hive hive))))
